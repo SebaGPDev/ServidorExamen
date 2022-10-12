5216c834194e8e6f51811f25ca636e163dcaf095
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-10-12 04:48:40 -0300

    changes and login incorporated

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const { encrypt, compare } = require("../helpers/handleBcrypt");
-const User = require("../models/User");
+const UserModel = require("../models/User");
 
 const authCtrl = {};
 
@@ -8,13 +8,37 @@ authCtrl.register = async (req, res) => {
     const { username, password, email } = req.body;
 
     const passwordHash = await encrypt(password);
-    const registerUser = await User.create({
+    const registerUser = await UserModel.create({
       username,
       email,
       password: passwordHash,
     });
     res.send({ data: registerUser });
-  } catch (err) {}
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+authCtrl.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      res.status(404);
+      res.send({ error: "User not found" });
+    }
+    const matchPassword = await compare(password, user.password);
+    if (matchPassword) {
+      res.send({ data: user });
+    }
+    if (!matchPassword) {
+      res.status(404);
+      res.send({ error: "Password mismatch" });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = authCtrl;
