c26325a4d72ecd42a028d1de4073073ce43a2617
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-10-12 04:49:10 -0300

    changes, login route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const { register } = require("../controllers/auth.controller");
+const { register, login } = require("../controllers/auth.controller");
 
-// router.post('/login', loginCtrl);
+router.post("/login', login");
 router.post("/register", register);
 
 module.exports = router;
