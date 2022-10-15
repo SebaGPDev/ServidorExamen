a958afda2110fa2743f76eee745aeb78f01a8912
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-10-15 10:15:05 -0300

    login incorporated

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,45 @@
+const UserModel = require("../models/Users");
+const tokenSing = require("../helpers/generateJWT");
+const bcrypt = require("bcrypt");
+
+const authCtrol = {};
+
+authCtrol.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return res.status(404).json({
+        Message:
+          "An error occurred while trying to login to the account (User not found)",
+      });
+    }
+
+    if (!user.isActive) {
+      return res.status(403).json({
+        Message:
+          "An error occurred while trying to login to the account (User not active)",
+      });
+    }
+
+    const matchPassword = bcrypt.compareSync(password, user.password);
+
+    if (!matchPassword) {
+      return res.status(404).json({
+        Message:
+          "An error occurred while trying to login to the account ( the password does not match )",
+      });
+    }
+
+    const tokenSession = await tokenSing({ uid: user._id });
+    return res.json({ tokenSession });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      Message: "An error occurred while trying to login to the account",
+    });
+  }
+};
+
+module.exports = authCtrol;
---
+++ src/routes/login.routes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+const { login } = require("../controllers/login.controller");
+
+router.post("/login", login);
+
+module.exports = router;
